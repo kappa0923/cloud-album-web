4bfc9d557df6d14721765574c5f4ec9caf8bf2ba
kappa0923 <tom.kappa0923@gmail.com>
2018-04-12 23:51:30 +0900

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-console": "off",
+    "non-used": "warn",
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
